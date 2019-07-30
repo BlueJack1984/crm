735d915efcf369611c541e83549650d949904fed
lushusheng <lushushen8@163.com>
2019-07-30 21:01:06 +0800

    commit files

--- admin/src/main/java/com/example/admin/AdminApplication.java
+++ admin/src/main/java/com/example/admin/AdminApplication.java
@@ -1,9 +1,15 @@
 package com.example.admin;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.example.admin", "com.example.core.service"})
+@EnableSwagger2
+@MapperScan(basePackages = {"com.example.core.dao"})
+@EnableEurekaClient
 public class AdminApplication {
 
     public static void main(String[] args) {
--- app/src/main/java/com/example/app/AppApplication.java
+++ app/src/main/java/com/example/app/AppApplication.java
@@ -3,11 +3,13 @@ package com.example.app;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(scanBasePackages = {"com.example.app", "com.example.core.service"})
 @EnableSwagger2
 @MapperScan(basePackages = {"com.example.core.dao"})
+@EnableEurekaClient
 public class AppApplication {
 
     public static void main( String[] args) {
