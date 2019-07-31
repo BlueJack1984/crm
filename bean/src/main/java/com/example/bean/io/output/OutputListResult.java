1bb7fe600d84066a89b3aaf3feb28984441923a2
lushusheng <lushushen8@163.com>
2019-07-31 18:46:16 +0800

    commit files

--- bean/src/main/java/com/example/bean/io/output/OutputListResult.java
+++ bean/src/main/java/com/example/bean/io/output/OutputListResult.java
@@ -6,6 +6,11 @@ import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
+
+/**
+ *
+ * @param <T>
+ */
 @Data
 @Slf4j
 public class OutputListResult<T> extends OutputResult<List<T>> {
